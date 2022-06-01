f4530a2958b8916fe926fb020fce03a40f809b61
rahul unni <rahul@rahulsmac.local>
2022-06-01 21:25:36 +0530

    Programs added

---
+++ RegularExpressionAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to C# Regular Expression");
+            //User First name validation
+            Firstname:
+            Console.WriteLine("\nNote : Your first name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your first name : ");
+            string fName = Console.ReadLine(); //storing first name entered by user in variable
+            bool checkFirstName = Validation.FirstNameValidation(fName); //Calling method to check first name is valid or not with argument
+            if (!checkFirstName)//If not valid then ask user to enter first name again
+                goto Firstname;
+
+            // User Last name validation
+            Lastname:
+            Console.WriteLine("\nNote : Your Last name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your last name : ");
+            string lName = Console.ReadLine(); //storing last name entered by user in variable
+            bool checkLastName = Validation.LastNameValidation(lName); //Calling method to check last name is valid or not with argument
+            if (!checkLastName)//If not valid then ask user to enter last name again
+                goto Lastname;
+
+            // User Email Validation
+            Email:
+            Console.WriteLine("\n\nEnter your email name : ");
+            string email = Console.ReadLine(); //storing email address entered by user in variable
+            bool checkEmail = Validation.EmailValidation(email); //Calling method to check email is valid or not with argument
+            if (!checkEmail)//If not valid then ask user to enter email again
+                goto Email;
+
+            //user Mobile Number validation
+            MobileNumber:
+            Console.WriteLine("\n\nEnter your Mobile Number : ");
+            string mNo = Console.ReadLine(); //storing mobile number entered by user in variable
+            bool checkMobileNo = Validation.MobileNumberValidation(mNo); //Calling method to check mobile number is valid or not with argument
+            if (!checkMobileNo)//If not valid then ask user to enter mobile number again
+                goto MobileNumber;
+
+            //user Password validation
+            Password:
+            Console.WriteLine("\nPassword Rules");
+            Console.WriteLine("Rule 1 : Minimum 8 Character");
+            Console.WriteLine("Rule 2 : Atleast one Uppercase letter");
+            Console.WriteLine("Rule 3 : Atleast one numeric number");
+            Console.WriteLine("Rule 4 :  Has Exactly One Special Character");
+            Console.WriteLine("\n\nEnter your Password : ");
+            string password = Console.ReadLine(); //storing password entered by user in variable
+            bool checkPassword = Validation.PasswordRule4Validation(password); //Calling method to check password follows all the rules or not with argument
+            if (!checkPassword)//If not follow then ask user to enter password again
+                goto Password;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegularExpressionAssignment/Validation.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionAssignment
+{
+    internal class Validation
+    {
+        public static bool FirstNameValidation(string fName) // Creating a method for First name validation
+        {                    
+            string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
+            //If name entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(fName, pattern)) 
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of first name");
+                return false;
+            }
+        }
+        public static bool LastNameValidation(string lName) // Creating a method for Last name validation
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Regex for last name validation
+            //If name entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(lName, pattern))
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of last name");
+                return false;
+            }
+        }
+        public static bool EmailValidation(string email) // Creating a method for Email validation
+        {
+            string pattern = "^[0-9a-zA-Z]+[./+_-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z0-9-]+[.][a-zA-Z]{2,}([.][a-zA-Z]{2,}){0,1}$"; // Regex for Email validation
+            //If email address entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine($"\nYour Email address \"{email}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour Email address \"{email}\" is not valid");
+                return false;
+            }
+        }
+        public static bool MobileNumberValidation(string mobileNo) // Creating a method for Mobile Number validation
+        {
+            string pattern = "^[9][1][ ][6-9][0-9]{9}$"; // Regex for Mobile number validation
+            //If Mobile Number entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(mobileNo, pattern))
+            {
+                Console.WriteLine($"\nYour Mobile Number \"{mobileNo}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour Mobile Number \"{mobileNo}\" is not valid");
+                return false;
+            }
+        }
+         public static bool PasswordRule1Validation(string password) // Creating a method for password validation
+        {
+            //Rule -1 Minimum 8 characters
+            string patternForRule1 = "^[0-9a-zA-Z@#$%^&*!+=]{8,}$"; // Regex for password validation rule 1
+            
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, patternForRule1))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
+        public static bool PasswordRule2Validation(string password) // Creating a method for password validation
+        {
+            //Rule - 2 Atleast 1 Capital Letter
+            string patternForRule2 = "^(?=.*[A-Z])[0-9a-zA-Z@#$%^&*!+=]{8,}$"; // Regex for password validation rule 2
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, patternForRule2))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
+        public static bool PasswordRule3Validation(string password) // Creating a method for password validation
+        {
+            //Rule - 3 Atleast 1 Numeric number
+            string patternForRule3 = "^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z@#$%^&*!+=]{8,}$"; // Regex for password validation rule 3
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, patternForRule3))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
+        public static bool PasswordRule4Validation(string password) // Creating a method for password validation
+        {
+            // Rule - 4 Hase exactly one special character
+            string pattern1 = @"^((?=.{8,}$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]{0,30}?[@~!#$%^&+*]{1}[a-zA-Z0-9]{0,30})$"; // Regex for password validation up to rule 4
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, pattern1))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
+    }
+}
+
